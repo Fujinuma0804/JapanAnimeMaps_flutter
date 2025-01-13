f89a28ba0873b375dcfaf54d2b27b5a5c9147197
sota-kawakami00 <websakusei02@gmail.com>
2025-01-13 14:25:54 +0900

    update for mail_sign_up2

--- lib/login_page/mail_sign_up2.dart
+++ lib/login_page/mail_sign_up2.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:intl/intl.dart';
 import 'package:parts/login_page/welcome_page/welcome_1.dart';
 
 class SecondSignUpPage extends StatefulWidget {
